80e16240728070026a6d90fd0428cc38196dff7a
trance1100 <trance1100@gmail.com>
2019-10-06 20:20:51 -0400

    update

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,8 +1,21 @@
-const  express = require('express');
+const express = require('express');
 
 const app = express();
 
-app.listen(3000, function() {
+
+app.get("/", function (req, res) {
+    res.send("<h1> Hello, World!</h1>");
+    console.log(req);
+});
+
+app.get("/contact", function(req, res)  {
+    res.send("<h1>Contact me at larry1011x@gmail.com</h1>");
+});
+
+app.get("/about", function(req, res) {
+    res.send("Just a programmer interested in code and also a computer support specialist.");
+});
+app.listen(3000, function () {
     console.log("Server started on port 3000");
-    
+
 });
