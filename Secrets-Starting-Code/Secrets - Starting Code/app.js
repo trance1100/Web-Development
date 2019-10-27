49d886c0953e4a26b6c311c51ddb15d4de6535e7
trance1100 <trance1100@gmail.com>
2019-10-27 15:01:26 -0400

    security

--- Secrets-Starting-Code/Secrets - Starting Code/app.js
+++ Secrets-Starting-Code/Secrets - Starting Code/app.js
@@ -4,8 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 const app = express();
 
 app.use(express.static("public"));
@@ -16,12 +15,11 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.API_KEY, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -37,13 +35,13 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/register", function(req, res){
+app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
-    newUser.save(function(err){
+    newUser.save(function (err) {
         if (err) {
             console.log(err);
         } else {
@@ -52,17 +50,19 @@ app.post("/register", function(req, res){
     });
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
-    User.findOne({email: username}, function(err, foundUser){
+    User.findOne({
+        email: username
+    }, function (err, foundUser) {
         if (err) {
             console.log(err);
-            
+
         } else {
             if (foundUser) {
-                if (foundUser.password === password){
+                if (foundUser.password === password) {
                     res.render("secrets");
                 }
             }
